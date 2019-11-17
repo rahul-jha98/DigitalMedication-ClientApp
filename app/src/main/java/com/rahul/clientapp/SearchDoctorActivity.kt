ad5e1064770c14534093da0b0a6348059eb555c4
RaiAnant <RaiAnant.iit2017103@iiita.ac.in>
2019-11-18 05:24:38 +0530

    adding doctor list view

---
+++ app/src/main/java/com/rahul/clientapp/SearchDoctorActivity.kt
@@ -0,0 +1,57 @@
+package com.rahul.clientapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.rahul.clientapp.R
+import com.rahul.clientapp.adapters.DoctorAdapter
+import com.rahul.clientapp.models.Doctor
+import kotlinx.android.synthetic.main.activity_search_doctor.*
+
+class SearchDoctorActivity : AppCompatActivity() {
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var viewAdapter: DoctorAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_doctor)
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = DoctorAdapter()
+
+        recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+        FirebaseDatabase.getInstance().getReference("/doctor/").addChildEventListener(object: ChildEventListener{
+            override fun onChildRemoved(p0: DataSnapshot) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                val doc = p0.getValue(Doctor::class.java)!!
+                viewAdapter.addItem(doc)
+
+            }
+
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+    }
+}
--- app/src/main/java/com/rahul/clientapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rahul/clientapp/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.rahul.clientapp.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.rahul.clientapp.R
+import com.rahul.clientapp.SearchDoctorActivity
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
